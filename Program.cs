741acdabbfce29c5fa49ad05eeee2e1e97ba3778
Vik <tsaawa2019@gmail.com>
2022-11-30 20:45:59 +0300

    ДОБАВЛЕН КОД ПРОГРАММЫ

--- Program.cs
+++ Program.cs
@@ -1,5 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;    
+using System.IO;
 
-int N=5;/*длина масссива*/
-string [] A=new string[N];
+    int N=5;/*длина массива*/
+    string [] A={"213","ed","dfdfdfd","","jjjjj","jkjkjk"};
+    string [] B=new string[N];
+
+    int  j = 0;
+
+    for(int i=0; i<=N; i++)
+    {
+        if(A[i].Length<=3)
+        {
+            B[j]=A[i];
+            j++;
+        }
+        
+    }
+    for(int i=0; i<=j; i++)
+        Console.Write($"{B[i]} ");
+ 
